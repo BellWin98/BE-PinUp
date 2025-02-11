90a1da15e5e24c2004c1822b79156151bd05553d
BellWin98 <jongseunghan@naver.com>
2025-02-11 09:39:37 +0900

    [Fix] 폴더 구조 변경으로 인한 LogAop 경로 수정

--- src/main/java/com/pinup/global/aop/LogAop.java
+++ src/main/java/com/pinup/global/aop/LogAop.java
@@ -12,8 +12,7 @@ import org.springframework.stereotype.Component;
 public class LogAop {
 
     // controller와 service method만 로그 적용
-//    @Around("execution(* com.pinup.controller..*(..)) || execution(* com.pinup.service..*(..))")
-    @Around("(execution(* com.pinup.controller..*(..)) || execution(* com.pinup.service..*(..))) && !execution(* com.pinup.domain.home.HomeController.*(..))")
+    @Around("(execution(* com.pinup.domain..controller..*(..)) || execution(* com.pinup.domain..service..*(..))) && !execution(* com.pinup.domain.home.HomeController.*(..))")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
         log.info("START: {}", joinPoint.toString());
         try {
