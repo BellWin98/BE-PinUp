0396a51fa1513f1d77d5051f1cbce022c3cbbdd7
Bellwin98 <jongseunghan@naver.com>
2025-02-15 19:47:18 +0900

    [Fix] Swagger 정보 및 날짜 포맷 수정

--- src/main/java/com/pinup/domain/member/dto/response/MemberReviewResponse.java
+++ src/main/java/com/pinup/domain/member/dto/response/MemberReviewResponse.java
@@ -18,21 +18,21 @@ import java.util.List;
 @Schema(title = "유저가 작성한 리뷰 조회 응답 DTO")
 public class MemberReviewResponse {
 
-    @Schema(description = "유저 개인정보")
+    @Schema(description = "리뷰 고유 ID")
     private Long reviewId;
-    @Schema(description = "유저 개인정보")
+    @Schema(description = "카카오맵 장소 ID")
     private String kakaoPlaceId;
-    @Schema(description = "유저 개인정보")
+    @Schema(description = "장소명")
     private String placeName;
-    @Schema(description = "유저 개인정보")
+    @Schema(description = "리뷰 내용")
     private String content;
-    @Schema(description = "유저 개인정보")
+    @Schema(description = "별점")
     private double starRating;
-    @Schema(description = "유저 개인정보")
+    @Schema(description = "리뷰 이미지 리스트")
     private List<String> reviewImageUrls;
-    @Schema(description = "유저 개인정보")
+    @Schema(description = "리뷰 작성일자")
     private String createdAt;
-    @Schema(description = "유저 개인정보")
+    @Schema(description = "장소 방문일자")
     private String visitedDate;
 
     public static MemberReviewResponse of(Review review) {
@@ -46,7 +46,7 @@ public class MemberReviewResponse {
                 .reviewImageUrls(review.getReviewImages().stream()
                         .map(ReviewImage::getUrl)
                         .toList())
-                .createdAt(DateTimeFormatter.ofPattern("yyyy-MM-dd").format(review.getCreatedAt()))
+                .createdAt(DateTimeFormatter.ofPattern("yy.MM.dd").format(review.getCreatedAt()))
                 .visitedDate(review.getVisitedDate())
                 .build();
     }
