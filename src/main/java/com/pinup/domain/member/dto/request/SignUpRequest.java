7e3c47962f78cd6a372259d03f7a13ec0b080079
Bellwin98 <jongseunghan@naver.com>
2025-02-19 22:32:28 +0900

    닉네임 pattern message 수정

--- src/main/java/com/pinup/domain/member/dto/request/SignUpRequest.java
+++ src/main/java/com/pinup/domain/member/dto/request/SignUpRequest.java
@@ -19,7 +19,7 @@ public class SignUpRequest {
     private String name;
 
     @NotEmpty(message = "닉네임은 필수 입력값입니다.")
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,12}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,12}$", message = "닉네임은 특수문자를 제외한 2~12자리여야 합니다.")
     private String nickname;
 
     @NotEmpty(message = "소셜 ID는 필수 입력값입니다.")
