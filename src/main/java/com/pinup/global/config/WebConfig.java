3e726f1fd90c1c2d7b993fd8bda162bbabbb7d4f
Bellwin98 <jongseunghan@naver.com>
2025-03-25 00:41:27 +0900

    [Fix] 인터셉터 제외 url 적용

--- src/main/java/com/pinup/global/config/WebConfig.java
+++ src/main/java/com/pinup/global/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(requestLoggingInterceptor).addPathPatterns("/api/**");
+        registry.addInterceptor(requestLoggingInterceptor).excludePathPatterns("/api/auth/**");
     }
 
     @Bean
