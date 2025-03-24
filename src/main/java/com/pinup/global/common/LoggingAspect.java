c98bb14e75f854d9a646826f40d520a58c99ef59
BellWin98 <jongseunghan@naver.com>
2025-03-24 17:36:01 +0900

    [Chore] health check api 로깅에서 제거

--- src/main/java/com/pinup/global/common/LoggingAspect.java
+++ src/main/java/com/pinup/global/common/LoggingAspect.java
@@ -2,6 +2,7 @@ package com.pinup.global.common;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
@@ -11,28 +12,28 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 
     // controller와 service method만 로그 적용
-/*    @Around("(execution(* com.pinup.domain..controller..*(..)) || execution(* com.pinup.domain..service..*(..))) && !execution(* com.pinup.domain.home.HomeController.*(..))")
+    @Around("(execution(* com.pinup.domain..service..*(..))) && !execution(* com.pinup.domain.home.HomeController.*(..))")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.info("START: {}", joinPoint.toString());
+        log.info("START: {}", joinPoint.getSignature().getName());
         try {
             return joinPoint.proceed();
         } finally {
-            log.info("END: {}", joinPoint.toString());
+            log.info("END: {}", joinPoint.getSignature().getName());
         }
-    }*/
-
-    @Before("execution(* com.pinup.domain..service..*(..)) && !execution(* com.pinup.domain.home.HomeController.*(..))")
-    public void logBeforeMethod(JoinPoint joinPoint) {
-        log.info("START: {}", joinPoint.getSignature().getName());
     }
 
-    @After("execution(* com.pinup.domain..service..*(..)) && !execution(* com.pinup.domain.home.HomeController.*(..))")
-    public void logAfterMethod(JoinPoint joinPoint) {
-        log.info("END: {}", joinPoint.getSignature().getName());
-    }
-
-    @AfterThrowing(pointcut = "execution(* com.pinup.domain..service..*(..)) && !execution(* com.pinup.domain.home.HomeController.*(..))", throwing = "ex")
-    public void logAfterThrowing(JoinPoint joinPoint, Throwable ex) {
-        log.error("Exception in Method: {}", joinPoint.getSignature().getName(), ex);
-    }
+//    @Before("execution(* com.pinup.domain..service..*(..)) && !execution(* com.pinup.domain.home.HomeController.*(..))")
+//    public void logBeforeMethod(JoinPoint joinPoint) {
+//        log.info("START: {}", joinPoint.getSignature().getName());
+//    }
+//
+//    @After("execution(* com.pinup.domain..service..*(..)) && !execution(* com.pinup.domain.home.HomeController.*(..))")
+//    public void logAfterMethod(JoinPoint joinPoint) {
+//        log.info("END: {}", joinPoint.getSignature().getName());
+//    }
+//
+//    @AfterThrowing(pointcut = "execution(* com.pinup.domain..service..*(..)) && !execution(* com.pinup.domain.home.HomeController.*(..))", throwing = "ex")
+//    public void logAfterThrowing(JoinPoint joinPoint, Throwable ex) {
+//        log.error("Exception in Method: {}", joinPoint.getSignature().getName(), ex);
+//    }
 }
