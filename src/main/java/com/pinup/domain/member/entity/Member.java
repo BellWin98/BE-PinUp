4da53d92eb7a3d7418ddbf911c5f7150b12dc008
Bellwin98 <jongseunghan@naver.com>
2025-03-31 15:52:36 +0900

    [Fix] 이메일 null 허용
    * 카카오 로그인 시 이메일을 못가져옴

--- src/main/java/com/pinup/domain/member/entity/Member.java
+++ src/main/java/com/pinup/domain/member/entity/Member.java
@@ -25,7 +25,6 @@ public class Member extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
