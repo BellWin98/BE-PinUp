b26075c75c1eac5ee46d9b1787807c6464fd127f
BellWin98 <jongseunghan@naver.com>
2025-04-11 13:33:11 +0900

    [Fix] 유저 정보 수정 API 수정
    * 기존 닉네임과 동일한 닉네임인 경우 중복여부 미검증
    * 소개글 필수값 제거
    * 기존 소개글과 동일한 소개글인 경우 업데이트 미실시

--- src/main/java/com/pinup/domain/member/dto/request/UpdateProfileRequest.java
+++ src/main/java/com/pinup/domain/member/dto/request/UpdateProfileRequest.java
@@ -11,7 +11,6 @@ public class UpdateProfileRequest {
     @Size(min = 2, max = 12, message = "닉네임은 2자 이상 12자 이하로 입력해주세요.")
     private String nickname;
 
-    @NotBlank(message = "수정할 소개글을 입력해주세요.")
     @Size(max = 150, message = "소개글은 150자 이내로 입력해주세요.")
     private String bio;
 }
